3fb254dcebea2c454961a2f40e8015d8617dcdb7
mahmoud <mahmoudabuyoussf20@gmail.com>
2022-11-02 07:43:16 +0200

    uplode

---
+++ js/main.js
@@ -0,0 +1,71 @@
+/*
+### Data Types
+
+  Boolean: True , False
+  Array  : ["firstName" , "Facebook.com"]
+  Object : {firstName: "Mahmoud", lastName: "Abuyoussef"}
+  String : "String Text"
+  Number : 12
+  Null
+  Symbol
+  NaN : Not a Number
+  Undefined
+*/
+/*
+var mainPrice = 500,
+    hasDiscount = true;
+
+if (hasDiscount === true) {
+  var mainPrice  = 250;
+} else {
+  var mainPrice  = 500;
+}
+document.getElementById("test").innerHTML = mainPrice;
+*/
+
+/*
+var price = 1000,
+    product = true;
+if (product === true) {
+  var price = "good";
+} else {
+  var price = 1000;
+}
+
+console.log(price);
+*/
+/*
+var elc = true;
+if(elc === true) {
+  console.log("the elc is turn on");
+} else {
+  console.log("the elc turn off");
+}
+*/
+/*
+var myName = "mahmoud",
+    myAge = 19,
+    myCountry = "Egypt";
+
+    document.getElementById("test").innerHTML =
+    "<span style=\"color:red; background-color: blue;\">" + "myName: " + myName + "</span><br>" +
+    " My Age: " + myAge + "<br>" +
+    " My Country " + myCountry;
+*/
+
+/*
+
+function say_hi_bye(myName, num) {
+  var zero =  "Bey",
+    one = "Hi";
+if (num === 0) {
+  return zero + " " +  myName;
+} else if (num === 1) {
+  return one + " " + myName
+}
+}
+
+console.log(say_hi_bye("mahmoud", 1));
+*/
+
+// simple calculater
